231743f50b90a30323ec78562973400ed750d532
yakikimi7-pixel <yakikimi7@gmail.com>
2026-02-20 12:58:49 -0400

    loader.user.js

---
+++ loader.user.js
@@ -0,0 +1,2 @@
+// @updateURL   https://raw.githubusercontent.com/yakikimi7-pixel/mis.extenciones/main/loader.user.js
+// @downloadURL https://raw.githubusercontent.com/yakikimi7-pixel/mis.extenciones/main/loader.user.js
